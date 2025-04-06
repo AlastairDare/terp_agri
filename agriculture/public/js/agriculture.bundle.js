631384cb7eae39a23339466dcae9233dc4df0af3
AlastairDare <dare.alastair@gmail.com>
2025-04-06 10:47:34 +0200

    fixed refs to missing js files in js bundle file

--- agriculture/public/js/agriculture.bundle.js
+++ agriculture/public/js/agriculture.bundle.js
@@ -2,24 +2,13 @@ import '../../agriculture/doctype/agriculture_analysis_criteria/agriculture_anal
 import '../../agriculture/doctype/agriculture_task/agriculture_task.js';
 import '../../agriculture/doctype/crop/crop.js';
 import '../../agriculture/doctype/crop_cycle/crop_cycle.js';
-import '../../agriculture/doctype/detected_disease/detected_disease.js';
 import '../../agriculture/doctype/disease/disease.js';
 import '../../agriculture/doctype/fertilizer/fertilizer.js';
-import '../../agriculture/doctype/fertilizer_content/fertilizer_content.js';
-import '../../agriculture/doctype/linked_location/linked_location.js';
-import '../../agriculture/doctype/linked_plant_analysis/linked_plant_analysis.js';
-import '../../agriculture/doctype/linked_soil_analysis/linked_soil_analysis.js';
-import '../../agriculture/doctype/linked_soil_texture/linked_soil_texture.js';
 import '../../agriculture/doctype/plant_analysis/plant_analysis.js';
-import '../../agriculture/doctype/plant_analysis_criteria/plant_analysis_criteria.js';
 import '../../agriculture/doctype/soil_analysis/soil_analysis.js';
-import '../../agriculture/doctype/soil_analysis_criteria/soil_analysis_criteria.js';
 import '../../agriculture/doctype/soil_texture/soil_texture.js';
-import '../../agriculture/doctype/soil_texture_criteria/soil_texture_criteria.js';
 import '../../agriculture/doctype/water_analysis/water_analysis.js';
-import '../../agriculture/doctype/water_analysis_criteria/water_analysis_criteria.js';
 import '../../agriculture/doctype/weather/weather.js';
-import '../../agriculture/doctype/weather_parameter/weather_parameter.js';
 
 
 
